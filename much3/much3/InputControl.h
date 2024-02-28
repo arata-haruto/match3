433bbcba62548c1dcc9b87f3659b88396200b9b3
arata-haruto <138739158+arata-haruto@users.noreply.github.com>
2024-02-28 17:22:45 +0900

    修正

--- much3/much3/InputControl.h
+++ much3/much3/InputControl.h
@@ -1,21 +1,23 @@
 #pragma once
-/*****************************
+
+/************************
 *マクロ定義
-*****************************/
+************************/
 
-/*****************************
+/************************
 *型定義
-*****************************/
+************************/
 
-/*****************************
+/************************
 *プロトタイプ宣言
-*****************************/
+************************/
+
 void Input_Initialize(void);
 void Input_Update(void);
 int Input_Escape(void);
 
 int GetOldKey(int key);
-int GetNowkey(int key);
+int Get_NowKey(int key);
 int GetKeyFlg(int key);
 int GetMousePositionX(void);
 int GetMousePositionY(void);
